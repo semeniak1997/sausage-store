f5c2c405f7582445a0bf93719058f8a4dfa74471
std-025-02 <>
2024-02-04 01:16:54 +0300

    add manual deploy for backend and frontend

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,11 +1,10 @@
 #! /bin/bash
 set -xe
 sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
-sudo rm -rf /home/student/sausage-store.jar
+sudo rm -rf /home/${DEV_HOST}/sausage-store.jar
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/${NEXUS_BACKEND}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo cp -rf ./sausage-store.jar /opt/sausage-store/bin/sausage-store.jar
-sudo rm -rf /home/student/sausage-store.jar
-sudo rm -rf /home/student/sausage-store-backend.service
+
 sudo systemctl daemon-reload
 sudo systemctl enable sausage-store-backend
 sudo systemctl restart sausage-store-backend

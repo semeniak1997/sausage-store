eab78031d32e36e1de609063146b22800a264609
std-025-02 <>
2024-02-03 23:17:37 +0300

    add deploy backend

---
+++ backend/deploy.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+set -xe
+sudo cp -rf sausage-store-backend.service /etc/systemd/system/sausage-store-backend.service
+sudo rm -rf /home/student/sausage-store.jar
+curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store.jar ${NEXUS_REPO_URL}/${NEXUS_BACKEND}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
+sudo cp -rf ./sausage-store.jar /var/jarservice/sausage-store.jar
+sudo systemctl daemon-reload
+sudo systemctl enable sausage-store-backend
+sudo systemctl restart sausage-store-backend

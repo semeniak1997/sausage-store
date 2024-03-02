15882c0f8f4d867e91d7b4ee4a20c6db2a44efe4
std-025-02 <>
2024-03-03 01:58:17 +0300

    fix deployment v2

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -3,23 +3,23 @@
 set -e
 
 sudo mv sausage-store-backend.service /opt/sausage-store/bin/backend_services/sausage-store-backend-${VERSION}.service
-sudo ln -sf /opt/sausage-store/bin/backend_services/sausage-store-backend-${VERSION}.service /etc/systemd/system/sausage-store-backend.service
+sudo ln -f /opt/sausage-store/bin/backend_services/sausage-store-backend-${VERSION}.service /etc/systemd/system/sausage-store-backend.service
 
-sudo echo -e 'PSQL_HOST="${PSQL_HOST}" \
-    \nPSQL_USER="${PSQL_USER}" \
-    \nPSQL_PASSWORD="${PSQL_PASS}" \
-    \nPSQL_PORT="${PSQL_PORT}" \
-    \nPSQL_DBNAME="${PSQL_DBNAME}" ' \
+sudo echo -e "PSQL_HOST=\"${PSQL_HOST}"\" \
+    \\nPSQL_USER=\"${PSQL_USER}"\"\
+    \\nPSQL_PASS=\"${PSQL_PASS}"\" \
+    \\nPSQL_PORT=\"${PSQL_PORT}\" \
+    \\nPSQL_DBNAME=\"${PSQL_DBNAME}\" \
     > ./db_creds
+
 sudo cp -f ./db_creds /root/.db_creds
 sudo rm -f ./db_creds
 
 curl -u ${NEXUS_REPO_USER}:${NEXUS_REPO_PASS} -o sausage-store-${VERSION}.jar ${NEXUS_REPO_URL}/${NEXUS_BACKEND}/com/yandex/practicum/devops/sausage-store/${VERSION}/sausage-store-${VERSION}.jar
 sudo mv ./sausage-store-${VERSION}.jar /opt/sausage-store/bin/artifacts/sausage-store-${VERSION}.jar
-sudo ln -sf /opt/sausage-store/bin/artifacts/sausage-store-${VERSION}.jar /opt/sausage-store/bin/sausage-store.jar
+sudo ln -f /opt/sausage-store/bin/artifacts/sausage-store-${VERSION}.jar /opt/sausage-store/bin/sausage-store.jar
 
-wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" \
-    -O root.crt
+wget "https://storage.yandexcloud.net/cloud-certs/CA.pem" -O root.crt
 
 sudo /usr/lib/jvm/java-16-openjdk-amd64/bin/keytool -import -alias YandexCA \
     -file root.crt \
@@ -31,3 +31,5 @@ rm -f root.crt
 sudo systemctl daemon-reload
 sudo systemctl enable sausage-store-backend
 sudo systemctl restart sausage-store-backend
+
+

decc35586c9b4550bab4f0697f905475d64b8883
std-025-02 <semeniak.nikita.1997@yandex.kz>
2024-03-14 17:18:35 +0300

    edit backend vars for deploy.sh

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -4,10 +4,10 @@ sudo docker login -u ${CI_REGISTRY_USER} -p${CI_REGISTRY_PASSWORD} ${CI_REGISTRY
 sudo docker network create -d bridge sausage_network || true
 sudo docker rm -f sausage-backend || true
 sudo docker run --rm -d --name sausage-backend \
-     --env SPRING_DATASOURCE_URL="${SPRING_DATASOURCE_URL}" \
-     --env SPRING_DATASOURCE_USERNAME="${SPRING_DATASOURCE_USERNAME}" \
-     --env SPRING_DATASOURCE_PASSWORD="${SPRING_DATASOURCE_PASSWORD}" \
-     --env SPRING_DATA_MONGODB_URI="${SPRING_DATA_MONGODB_URI}" \
+     --env SPRING_DATASOURCE_URL="jdbc:postgresql://${PSQL_HOST}:${PSQL_PORT}/${PSQL_DBNAME}" \
+     --env SPRING_DATASOURCE_USERNAME="${PSQL_USER}" \
+     --env SPRING_DATASOURCE_PASSWORD="${PSQL_PASS}" \
+     --env SPRING_DATA_MONGODB_URI="mongodb://${MONGO_USER}:${MONGO_PASS}@${MONGO_HOST}:${MONGO_PORT}/${MONGO_DBNAME}?tls=true" \
      --env REPORT_PATH=/app/report \
      --env SPRING_FLYWAY_ENABLED=false \
      --network=sausage_network \
